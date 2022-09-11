57d31023ba28d25b3091513c68e792db73817d66
Dawidbtc <chudzikdavid1337@gmail.com>
2022-09-11 17:46:28 -0400

    Init Commit

---
+++ app/src/main/java/com/example/simpletodo/TaskItemAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.simpletodo
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TaskItemAdapter(val listOfTasks: List<String>, val longClickListener: OnLongClickListener) : RecyclerView.Adapter<TaskItemAdapter.ViewHolder>(){
+
+    interface OnLongClickListener{
+        fun onItemLongClicked(position: Int)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        val taskView = inflater.inflate(android.R.layout.simple_list_item_1,parent,false)
+        return ViewHolder(taskView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = listOfTasks.get(position)
+
+        holder.textView.text = item
+    }
+
+    override fun getItemCount(): Int {
+        return listOfTasks.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val textView : TextView
+
+        init{
+            textView = itemView.findViewById(android.R.id.text1)
+            itemView.setOnLongClickListener{
+                longClickListener.onItemLongClicked(adapterPosition)
+                true
+            }
+        }
+    }
+}
